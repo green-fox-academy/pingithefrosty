5a84723eecf19f447452f3e46f691a7d22771c87
pingithefrosty <barni.kadar@gmail.com>
2017-11-28 17:19:28 +0100

    halfway to day 2

---
+++ week2/day2/double_items/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+// - Create an array variable named `ag`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
+        int[] ag = {3,4,5,6,7};
+        for (int i = 0; i < ag.length; i++){
+            ag[i] = ag[i]*2;
+        }
+        System.out.println(Arrays.toString(ag));
+
+    }
+}

e71215bc252ddf79c6a55211aff68d5bda0f160f
pingithefrosty <barni.kadar@gmail.com>
2017-11-28 18:01:54 +0100

    halfway to day 2

--- week2/day2/append_a/src/Main.java
+++ week2/day2/append_a/src/Main.java
@@ -1,14 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        String am = "kuty";
-        am = appendA(am);
-        System.out.print(am);
+// - Create an array variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
+        String[] nimals = {"kuty","macsk","cic"};
+        for (int i = 0; i<nimals.length; i++){
+            nimals[i]=nimals[i] + "a";
+        }
+        System.out.print(Arrays.toString(nimals));
     }
-    public static String appendA(String am){
-        return am + "a";
-    }
-// - Create a string variable named `am` and assign the value `kuty` to it
-// - Write a function called `appendA` that gets a string as an input
-//   and appends an 'a' character to its end
-// - Print the result of `appendA(am)`
 }
--- week2/day2/colors/src/Main.java
+++ week2/day2/colors/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
 public class Main {
-
     public static void main(String[] args) {
+        String[][] colors = new String[3][5];
+        colors[0][0] = "lime";
+        colors[0][1] = "forest green";
+        colors[0][2] = "olive";
+        colors[0][3] = "pale green";
+        colors[0][4] = "spring green";
+        colors[1][0] = "orange red";
+        colors[1][1] = "red";
+        colors[1][2] = "tomato";
+        colors[2][0] = "orchid";
+        colors[2][1] = "violet";
+        colors[2][2] = "pink";
+        colors[2][3] = "hot pink";
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.print(colors[i][j] + "\t\t\t");
+            }
+            System.out.println("");
+        }
+    }
+}
 // - Create a two dimensional array
 //   which can contain the different shades of specified colors
 // - In `colors[0]` store the shades of green:
@@ -9,6 +30,3 @@ public class Main {
 //   `"orange red", "red", "tomato"`
 // - In `colors[2]` store the shades of pink:
 //   `"orchid", "violet", "pink", "hot pink"`
-        
-    }
-}

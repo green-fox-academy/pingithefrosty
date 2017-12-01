8a55a488ffff5ee6fb3d099e72c0d7673e508280
pingithefrosty <barni.kadar@gmail.com>
2017-12-01 11:12:21 +0100

    day 4 second task

---
+++ week2/day4/find_the_substring/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
+/*
+Create a function that takes two strings as a parameter
+Returns the starting index where the second one is starting in the first one
+Returns -1 if the second string is not in the first one
+ */
--- week2/day4/sort_that_list/src/Main.java
+++ week2/day4/sort_that_list/src/Main.java
@@ -3,10 +3,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner asd = new Scanner(System.in);
+        ArrayList<String> input = new ArrayList<>();
         String userinput = asd.nextLine();
-        
+        Boolean userinput2 = asd.nextBoolean();
+        String[] parts = userinput.split(" ");
+        for (int i = 0; i < parts.length; i++) {
+            input.add(parts[i]);
+        }
+        System.out.println();
+        if (userinput2 == true) {
+            Collections.sort(input, Collections.reverseOrder());
+            System.out.println(input);
+        } else {
+            Collections.sort(input);
+            System.out.println(input);
+        }
     }
 }
+
 /*
 Create a function that takes a list of numbers as parameter
 Returns a list where the elements are sorted in ascending numerical order

bd2625921631dd7317fcd8f64d9199902908a8af
pingithefrosty <barni.kadar@gmail.com>
2017-12-03 13:21:14 +0100

    day 5 last task

---
+++ week2/day5/josephus_problem/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Josephus cave. How many people are inside?");
+        Scanner asd = new Scanner(System.in);
+        int input = asd.nextInt();
+        int powerTwo = largestPowerOfTwo(input);
+        int result = 2*(input-powerTwo)+1;
+        System.out.println("You should stand at place: "+result+".");
+    }
+
+    private static int largestPowerOfTwo(int n) {
+            if((n & n-1) == 0){
+            }
+            while((n & n-1) != 0){
+                n = n & n-1;
+            }
+            return n;
+    }
+}

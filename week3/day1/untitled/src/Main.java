638816e3772b0508ae16cf93dac2a7c5792cbe5b
pingithefrosty <barni.kadar@gmail.com>
2017-12-04 18:38:49 +0100

    week3 day1 task11 first half

--- week3/day1/untitled/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-  public static void main(String[] args) {
-    String original = "ghijkl/ a";
-    for (int i = 0; i < original.length(); i++) {
-      System.out.println ((char)(((int) original.charAt(i))-1));
-    }
-  }
-}

2133ff7f4ad33d1f3a7c7042b0b75515f068616b
pingithefrosty <barni.kadar@gmail.com>
2017-12-04 14:09:43 +0100

    week3 day1 task5

--- week3/day1/E05/src/WriteMultipleLines.java
+++ week3/day1/E05/src/WriteMultipleLines.java
@@ -1,13 +1,28 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class WriteMultipleLines {
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please input the name and path of the file");
+    String inputPath = scanner.nextLine();
+    System.out.println("Please input the content of the file");
+    String inputWord = scanner.nextLine();
+    System.out.println("Please input how many lines the file should have");
+    int inputNumber = scanner.nextInt();
+    Path path = Paths.get(inputPath);
+    try {
+      List<String> content = new ArrayList();
+      for (int i = 0; i < inputNumber; i++) {
+        content.add(inputWord);
+      }
+      Files.write(path, content);
+    } catch (Exception e) {
+      System.out.println("Unable to write file.");
+    }
   }
 }
-// Create a function that takes 3 parameters: a path, a word and a number,
-// than it should write to a file.
-// The path parameter should be a string, that describes the location of the file.
-// The word parameter should be a string, that will be written to the file as lines
-// The number paramter should describe how many lines the file should have.
-// So if the word is "apple" and the number is 5, than it should write 5 lines
-// to the file and each line should be "apple"
-// The function should not raise any error if it could not write the file.

c92b16961b0e11992cf87c281f7ef15d31520f98
pingithefrosty <barni.kadar@gmail.com>
2017-12-07 14:33:31 +0100

    week3 day4 task7

--- week3/day4/07/src/String1.java
+++ week3/day4/07/src/String1.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class String1 {
-  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter a string: ");
+    String newString = scanner.nextLine();
+    System.out.println(convertxy(newString));
+  }
+
+  private static String convertxy(String n) {
+    if (n.length() == 0) {
+      return "";
+    }
+    if (n.charAt(0) == 'x') {
+      return "y" + convertxy(n.substring(1, n.length()));
+    } else {
+      return n.charAt(0) + convertxy(n.substring(1, n.length()));
+    }
+  }
 }

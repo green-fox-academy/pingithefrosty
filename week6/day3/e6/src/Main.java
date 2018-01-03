393f4dce5a5f9a8a1246fd40ae7f78f7652aeef5
pingithefrosty <barni.kadar@gmail.com>
2018-01-03 16:07:56 +0100

    w6d3 e5

---
+++ week6/day3/e6/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    // Exercise 6
+    //Write a Stream Expression to find the frequency of characters in a given string!
+        
+  }
+}

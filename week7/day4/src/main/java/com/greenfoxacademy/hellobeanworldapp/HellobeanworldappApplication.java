4cf06e2ba7e6413a95d6b86541d13e080d2ba8ec
pingithefrosty <barni.kadar@gmail.com>
2018-01-11 23:36:25 +0100

    w2d4 init

--- week7/day4/src/main/java/com/greenfoxacademy/hellobeanworldapp/HellobeanworldappApplication.java
+++ week7/day4/src/main/java/com/greenfoxacademy/hellobeanworldapp/HellobeanworldappApplication.java
@@ -1,12 +1,22 @@
 package com.greenfoxacademy.hellobeanworldapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellobeanworldappApplication {
+public class HellobeanworldappApplication implements CommandLineRunner {
+
+	@Autowired
+  Printer p;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellobeanworldappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+    p.log("How cool is that?");
 	}
 }

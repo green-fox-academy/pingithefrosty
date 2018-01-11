d326440994e0817bf2ec3851fc43911249aa6272
pingithefrosty <barni.kadar@gmail.com>
2018-01-11 22:04:39 +0100

    w7d2 bankofsimba last 2e started

--- week7/day2/src/main/java/com/greenfoxacademy/bankofsimba/controllers/Controller.java
+++ week7/day2/src/main/java/com/greenfoxacademy/bankofsimba/controllers/Controller.java
@@ -2,8 +2,9 @@ package com.greenfoxacademy.bankofsimba.controllers;
 
 import com.greenfoxacademy.bankofsimba.models.BankAccount;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,26 +31,43 @@ public class Controller {
 
   @RequestMapping("/accounts")
   public String list(Model model) {
-    accountList.add(new BankAccount("Simba", 2000, "lion", true, "Good One"));
-    accountList.add(new BankAccount("Timon", 1000, "meercat", false, "Good One"));
-    accountList.add(new BankAccount("Pumbaa", 3000, "warthog", false, "Good One"));
-    accountList.add(new BankAccount("Rafiki", 8000, "baboon", false, "Good One"));
-    accountList.add(new BankAccount("Scar", 1500, "lion", false, "Bad Guy"));
+    if (accountList.size() == 0) {
+      accountList.add(new BankAccount("Simba", 2000, "lion", true, "Good One"));
+      accountList.add(new BankAccount("Timon", 1000, "meercat", false, "Good One"));
+      accountList.add(new BankAccount("Pumbaa", 3000, "warthog", false, "Good One"));
+      accountList.add(new BankAccount("Rafiki", 8000, "baboon", false, "Good One"));
+      accountList.add(new BankAccount("Scar", 1500, "lion", false, "Bad Guy"));
+    }
     model.addAttribute("accountList", accountList);
     return "accounts";
   }
 
-    public Model modelConstructor(Model model, BankAccount bankAccount) {
-      model.addAttribute("name", bankAccount.getName());
-      model.addAttribute("balance", bankAccount.getBalance());
-      model.addAttribute("animalType", bankAccount.getAnimalType());
-      model.addAttribute("status", bankAccount.isStatus());
-      model.addAttribute("affiliation", bankAccount.getAffiliation());
-      return model;
+  @RequestMapping(value = "/accounts/raise", method = RequestMethod.POST)
+  public String raise(Model model, @ModelAttribute BankAccount bankAccount) {
+    for (BankAccount bankAccount1 : accountList) {
+      if (bankAccount1.isStatus()) {
+        bankAccount1.setBalance((int) (bankAccount1.getBalance() + 100));
+      } else {
+        bankAccount1.setBalance((int) (bankAccount1.getBalance() + 10));
+      }
     }
+    model.addAttribute("accountList", accountList);
+    return "accounts";
+  }
+
+  @RequestMapping (value = "/accounts/add", method = RequestMethod.POST)
+  public String add(Model model, @ModelAttribute BankAccount bankAccount) {
+    accountList.add(bankAccount);
+    model.addAttribute("accountList", accountList);
+    return "accounts";
+  }
+
+  public Model modelConstructor(Model model, BankAccount bankAccount) {
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("animalType", bankAccount.getAnimalType());
+    model.addAttribute("status", bankAccount.isStatus());
+    model.addAttribute("affiliation", bankAccount.getAffiliation());
+    return model;
+  }
 }
-
-
-/*
-  In your controller create the first endpoint called /show
-    Instantiate a new BankAccount("Simba", "2000", "lion")*/

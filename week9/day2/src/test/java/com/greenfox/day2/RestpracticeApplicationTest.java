956f646291730f133c93d61400427bef6024a9f6
pingithefrosty <barni.kadar@gmail.com>
2018-01-24 15:39:17 +0100

    w9d3 tests for d2 finished

--- week9/day2/src/test/java/com/greenfox/day2/RestpracticeApplicationTest.java
+++ week9/day2/src/test/java/com/greenfox/day2/RestpracticeApplicationTest.java
@@ -1,26 +1,20 @@
 package com.greenfox.day2;
 
 import com.example.greenfox.restpractice.restpractice.RestpracticeApplication;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 @RunWith(SpringRunner.class)
@@ -39,28 +33,6 @@ public class RestpracticeApplicationTest {
     this.mockMvc = webAppContextSetup(webApplicationContext).build();
   }
 
-  /*@Test
-  public void testUnsuccessfulSignUp() throws Exception {
-    mockMvc.perform(post("/user/signup")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content("{\"email\": \"name@example.com\", \"password\": \"12345\"}"))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType("application/json;charset=UTF-8"))
-        .andExpect(jsonPath("$.result", is("fail")))
-        .andExpect(jsonPath("$.message", is("email address already exists")));
-  }*/
-
-  @Test
-  public void doUntilTest() throws Exception {
-    mockMvc.perform(
-        post("/dountil/sum")
-            .contentType("application/json;charset=UTF-8")
-            .content("{\"until\": 10}"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.received", is(5)))
-        .andExpect(jsonPath("$.result", is(15)));
-  }
-
   @Test
   public void doublingTest() throws Exception {
     mockMvc.perform(
@@ -69,6 +41,68 @@ public class RestpracticeApplicationTest {
         .andExpect(content().contentType("application/json;charset=UTF-8"))
         .andExpect(jsonPath("$.received", is(15)))
         .andExpect(jsonPath("$.result", is(30)));
+  }
 
+  @Test
+  public void greeterTest() throws Exception {
+    mockMvc.perform(
+        get("/greeter?name=Adam&title=programmer"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json;charset=UTF-8"))
+        .andExpect(jsonPath("$.welcome_message", is("Oh, hi there Adam, my dear programmer!")));
+  }
+
+  @Test
+  public void appendaTest() throws Exception {
+    mockMvc.perform(
+        get("/appenda/kuty"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json;charset=UTF-8"))
+        .andExpect(jsonPath("$.appended", is("kutya")));
+  }
+
+  @Test
+  public void doUntilTest() throws Exception {
+    mockMvc.perform(
+        post("/dountil/sum")
+            .contentType("application/json;charset=UTF-8")
+            .content("{\"until\": 5}"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.result", is(15)));
+    mockMvc.perform(
+        post("/dountil/factor")
+            .contentType("application/json;charset=UTF-8")
+            .content("{\"until\": 5}"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.result", is(120)));
+  }
+
+  @Test
+  public void arrayHandlerTest() throws Exception {
+    //int[] resultIntArray = {1, 2, 5, 10};
+    mockMvc.perform(
+        post("/arrays")
+            .contentType("application/json;charset=UTF-8")
+            .content("{\"what\": \"sum\", \"numbers\": [1, 2, 5, 10]}"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.result", is(18)));
+    mockMvc.perform(
+        post("/arrays")
+            .contentType("application/json;charset=UTF-8")
+            .content("{\"what\": \"multiply\", \"numbers\": [1, 2, 5, 10]}"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.result", is(100)));
+/*    mockMvc.perform(
+        post("/arrays")
+            .contentType("application/json;charset=UTF-8")
+            .content("{\"what\": \"double\", \"numbers\": [1, 2, 5, 10]}"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.result", is(resultIntArray)));*/
+    mockMvc.perform(
+        post("/arrays")
+            .contentType("application/json;charset=UTF-8")
+            .content("{\"what\": \"divide\", \"numbers\": [1, 2, 5, 10]}"))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.error", is("Please provide what to do with the numbers!")));
   }
 }
